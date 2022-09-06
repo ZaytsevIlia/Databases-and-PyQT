d2d0a684c5c9328dea455fce8bec5bb032901e89
Ilya <zaytsevilia.a@gmail.com>
2022-09-06 22:45:29 +0300

    lesson_1 homework

---
+++ lesson_1/task_1_2_3.py
@@ -0,0 +1,55 @@
+import platform
+from ipaddress import ip_address
+from tabulate import tabulate
+from subprocess import Popen, PIPE
+
+result = {'Узел доступен': '', 'Узел недоступен': ''}
+
+
+def host_ping(ip_dict):
+    for ip in ip_dict:
+        param = '-n' if platform.system().lower() == 'windows' else '-c'
+        args = ['ping', param, '1', str(ip)]
+        reply = Popen(args, stdout=PIPE)
+
+        if reply.wait() == 0:
+            result['Узел доступен'] += f'{ip}\n'
+        else:
+            result['Узел недоступен'] += f'{ip}\n'
+    return result
+
+
+def create_ip_dict():
+    ip_dict = []
+    ip_addr = input('Введите ip адрес: ')
+    try:
+        ipv4 = ip_address(ip_addr)
+    except ValueError:
+        raise Exception('Некорректный ip адрес')
+    last_part_ip = int(ip_addr.split('.')[3])
+    num_ip = int(input('Введите количество ip: '))
+    while True:
+        if last_part_ip + num_ip > 256:
+            print('Максимальное число хостов не должно превышать 256.')
+            num_ip = int(input('Введите количество ip: '))
+        else:
+            break
+    for i in range(num_ip):
+        ip_dict.append(str(ipv4 + i))
+    return ip_dict
+
+
+if __name__ == '__main__':
+    """
+    Для решения первой задачи
+    """
+    # my_dict = ['192.168.8.1', '8.8.8.8', 'yandex.ru', 'google.com',
+    #            '0.0.0.1', '0.0.0.2', '0.0.0.3', '0.0.0.4', '0.0.0.5',
+    #            '0.0.0.6', '0.0.0.7', '0.0.0.8', '0.0.0.9', '0.0.1.0']
+    # host_ping(my_dict)
+    # print(tabulate([result], headers='keys', tablefmt='grid'))
+    """
+    Для решения второй и третьей задачи
+    """
+    host_ping(create_ip_dict())
+    print(tabulate([result], headers='keys', tablefmt='grid'))
